ad5153865e2db5eada52f27b598ac1e9b6b67f65
Eder Jr <ederjuninho2003@gmail.com>
2024-04-17 17:54:18 -0300

    feat: create folders models, controllers and middlewares

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,6 +4,7 @@ const dbPassword = process.env.DB_PASS
 
 const conn = async () => {
   try {
+
     const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.zi3qld3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
 
     console.log("Conectou ao banco!")
