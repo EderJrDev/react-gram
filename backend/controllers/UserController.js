bed379bbcb2dafa6e5cdec27c566c38b498b1653
Eder Jr <ederjuninho2003@gmail.com>
2024-05-02 13:53:28 -0300

    feat: add function getById and create folders to photos

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -119,9 +119,29 @@ const update = async (req, res) => {
 
 }
 
+// Get user by id
+const getUserById = async (req, res) => {
+  const { id } = req.params
+
+  try {
+    const user = await User.findById(mongoose.Types.ObjectId(id)).select("-password")
+    // check if user existis
+    if (!user) {
+      res.status(404).json({ errors: ["Usuário não encontrado"] });
+      return;
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(404).json({ errors: ["Usuário não encontrado"] });
+  }
+
+}
+
 module.exports = {
   register,
   login,
   getCurrentUser,
-  update
+  update,
+  getUserById
 }
