76ecbce5f2d497411bf749bee51ecdc64922f764
EderJrDev <ederjuninho2003@gmail.com>
2024-11-26 20:38:32 -0300

    feat: add form to create acconunt and styles to page register and global css

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,14 +1,49 @@
 import "./Auth.css"
-
+import { Link } from 'react-router-dom'
 //components
 // hooks
 
-
 const Register = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
   return (
-    <div>
+    <div id="register">
       <h2>ReactGram</h2>
-      <p>Cadastre-se para ver as fotos dos seus amigos.</p>
+      <p className="subtitle">Cadastre-se para ver as fotos dos seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Nome"
+          // onChange={(e) => setName(e.target.value)}
+          // value={name}
+        />
+        <input
+          type="email"
+          placeholder="E-mail"
+          // onChange={(e) => setEmail(e.target.value)}
+          // value={email}
+        />
+        <input
+          type="password"
+          placeholder="Senha"
+          // onChange={(e) => setPassword(e.target.value)}
+          // value={password}
+        />
+        <input
+          type="password"
+          placeholder="Confirme a senha"
+          // onChange={(e) => setConfirmPassword(e.target.value)}
+          // value={confirmPassword}
+        />
+        {/* {!loading && <input type="submit" value="Cadastrar" />}
+        {loading && <input type="submit" disabled value="Aguarde..." />}
+        {error && <Message msg={error} type="error" />} */}
+      </form>
+      <p>
+        Já tem conta? <Link to="/login">Clique aqui</Link>
+      </p>
     </div>
   )
 }
